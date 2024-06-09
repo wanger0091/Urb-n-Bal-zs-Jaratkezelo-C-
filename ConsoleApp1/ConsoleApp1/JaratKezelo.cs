8f1ecfb7d52533347fce12ab7034d9c21e78ef4b
wanger0091 <ubalazs30@gmail.com>
2024-06-09 11:33:05 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/JaratKezelo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JaratKezeloProject
+{
+    public class JaratKezelo
+    {
+        private class Jarat
+        {
+            public string JaratSzam { get; }
+            public string RepterHonnan { get; }
+            public string RepterHova { get; }
+            public DateTime Indulas { get; }
+            public int OsszesKeses { get; private set; }
+
+            public Jarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+            {
+                JaratSzam = jaratSzam;
+                RepterHonnan = repterHonnan;
+                RepterHova = repterHova;
+                Indulas = indulas;
+                OsszesKeses = 0;
+            }
+
+            public void AddKeses(int keses)
+            {
+                if (OsszesKeses + keses < 0)
+                {
+                    throw new ArgumentException("A késés nem lehet negatív.");
+                }
+                OsszesKeses += keses;
+            }
+
+            public DateTime GetTenyelegesIndulas()
+            {
+                return Indulas.AddMinutes(OsszesKeses);
+            }
+        }
+
+        private readonly Dictionary<string, Jarat> jaratok = new Dictionary<string, Jarat>();
+
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            if (jaratok.ContainsKey(jaratSzam))
+            {
+                throw new ArgumentException("A járatszám már létezik.");
+            }
+
+            var jarat = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
+            jaratok.Add(jaratSzam, jarat);
+        }
+
+        public void Keses(string jaratSzam, int keses)
+        {
+            if (!jaratok.ContainsKey(jaratSzam))
+            {
+                throw new ArgumentException("Nem létező járatszám.");
+            }
+
+            var jarat = jaratok[jaratSzam];
+            jarat.AddKeses(keses);
+        }
+
+        public DateTime MikorIndul(string jaratSzam)
+        {
+            if (!jaratok.ContainsKey(jaratSzam))
+            {
+                throw new ArgumentException("Nem létező járatszám.");
+            }
+
+            return jaratok[jaratSzam].GetTenyelegesIndulas();
+        }
+
+        public List<string> JaratokRepuloterrol(string repter)
+        {
+            List<string> eredmeny = new List<string>();
+
+            foreach (var jarat in jaratok.Values)
+            {
+                if (jarat.RepterHonnan == repter)
+                {
+                    eredmeny.Add(jarat.JaratSzam);
+                }
+            }
+
+            return eredmeny;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using JaratKezeloProject;
+
+namespace TestJaratKezeloProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestUjJarat();
+            TestKeses();
+            TestMikorIndul();
+            TestJaratokRepuloterrol();
+
+            Console.WriteLine("Minden teszt sikeresen lefutott!");
+        }
+
+        static void TestUjJarat()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("J123", "BUD", "NYC", new DateTime(2023, 6, 7, 12, 0, 0));
+
+            var result = jaratKezelo.MikorIndul("J123");
+            if (result != new DateTime(2023, 6, 7, 12, 0, 0))
+            {
+                throw new Exception("Új járat tesztelése sikertelen.");
+            }
+
+            try
+            {
+                jaratKezelo.UjJarat("J123", "BUD", "LAX", new DateTime(2023, 6, 8, 12, 0, 0));
+                throw new Exception("Új járat tesztelése sikertelen...");
+            }
+            catch (ArgumentException)
+            {
+               
+            }
+        }
+
+        static void TestKeses()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("J123", "BUD", "NYC", new DateTime(2023, 6, 7, 12, 0, 0));
+            jaratKezelo.Keses("J123", 30);
+
+            var result = jaratKezelo.MikorIndul("J123");
+            if (result != new DateTime(2023, 6, 7, 12, 30, 0))
+            {
+                throw new Exception("TestKeses sikertelen.");
+            }
+
+            try
+            {
+                jaratKezelo.Keses("J123", -60);
+                throw new Exception("TestKeses sikertelen, nem lehet negativ.");
+            }
+            catch (ArgumentException)
+            {
+                
+            }
+        }
+
+        static void TestMikorIndul()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("J123", "BUD", "NYC", new DateTime(2023, 6, 7, 12, 0, 0));
+            jaratKezelo.Keses("J123", 10);
+            jaratKezelo.Keses("J123", 20);
+
+            var result = jaratKezelo.MikorIndul("J123");
+            if (result != new DateTime(2023, 6, 7, 12, 30, 0))
+            {
+                throw new Exception("TestMikorIndul sikertelen.");
+            }
+        }
+
+        static void TestJaratokRepuloterrol()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("J123", "BUD", "NYC", new DateTime(2023, 6, 7, 12, 0, 0));
+            jaratKezelo.UjJarat("J124", "BUD", "LAX", new DateTime(2023, 6, 8, 12, 0, 0));
+            jaratKezelo.UjJarat("J125", "LAX", "NYC", new DateTime(2023, 6, 9, 12, 0, 0));
+
+            var result = jaratKezelo.JaratokRepuloterrol("BUD");
+
+            if (!result.Contains("J123") || !result.Contains("J124") || result.Contains("J125"))
+            {
+                throw new Exception("TestJaratokRepuloterrol sikertelen.");
+            }
+        }
+    }
+}
